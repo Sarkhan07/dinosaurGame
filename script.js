5ce59c8bfe7c8fc3d0a04683658de523972a0e50
Sarkhan <abdullayevsarxan92@gmail.com>
2023-05-18 22:37:02 +0400

    init: starting of project

---
+++ script.js
@@ -0,0 +1,46 @@
+const dino = document.getElementById('dino');
+const cactus = document.getElementById('cactus');
+const game = document.querySelector('.game');
+
+document.addEventListener('keydown', function (event) {
+    jump();
+});
+
+game.addEventListener('touchstart', function (event) {
+    jump();
+});
+
+function jump() {
+    if (dino.classList != 'jump') {
+        dino.classList.add('jump');
+    }
+    setTimeout(() => {
+        dino.classList.remove('jump');
+    }, 300);
+}
+
+// function jump() {
+//     if (!dino.classList.contains('jump')) {
+//         dino.classList.add('jump');
+//         setTimeout(function () {
+//             dino.classList.remove('jump');
+//         }, 500);
+//     }
+// }
+
+let isAlive = setInterval(function () {
+    let dinoTop = parseInt(
+        window.getComputedStyle(dino).getPropertyValue('top')
+    );
+
+    let barrierLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue('left')
+    );
+
+    // let time = window.getComputedStyle(barrier).animationIterationCount;
+
+    if (barrierLeft < 120 && barrierLeft > 0 && dinoTop >= 250) {
+        alert('Game over!!!');
+        // time = 50;
+    }
+}, 10);
